d6dac7d2361fda6e37fb6be97d3c83d026e12910
AaronPrince <princeborn1999@gmail.com>
2022-09-05 19:04:35 +0800

    fix the splice

--- removeElement.js
+++ removeElement.js
@@ -5,12 +5,14 @@
  */
 var removeElement = function (nums, val) {
   let arr = nums;
+  
   for (a of arr) {
     if (a == val) {
-      arr.splice(a, 1);
+      arr.splice(arr.indexOf(a),1);      
     }
   }
   console.log(arr);
+  return(arr);
 };
 
-removeElement([2, 3, 3, 2], 2);
+removeElement([2,3,3,2], 2);
