176e7b5be89dcca35c5e207e6b737af3046bdcd5
AaronPrince <princeborn1999@gmail.com>
2022-09-08 12:10:46 +0800

    tree2str0907

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,18 +1,24 @@
+//from Sorted Array
 
 var removeDuplicates = function(nums) {
     let numArr = nums;
-    let tmp = 0;
+    let tmpNum = 0;
+    let originNumLen = numArr.length;
     for(a of numArr){
-        tmp = a;
-        for(let i=numArr.indexOf(a); i<numArr.length; i++){
-            if(i === tmp){
-                numArr.splice(i,1,"_");
-                console.log(i,tmp);
-                //用count寫好了
+        tmpNum = a;
+        for(let i=numArr.indexOf(a)+1; i<numArr.length; i++){
+            //console.log("目前元素: ",a,"指向元素: ",numArr[i],"要核定元素: ",tmpNum);
+            if(numArr[i] == tmpNum){
+                numArr.splice(i,1); 
             }
         }
     }
+    numArr.sort();
+    // for(let j=numArr.length; j < originNumLen; j++){
+    //     numArr.push("_");
+    // }
     console.log(numArr);
+    return numArr;
 };
 
-removeDuplicates([1,1,2]);
+removeDuplicates([1,1,3,2,2]);
